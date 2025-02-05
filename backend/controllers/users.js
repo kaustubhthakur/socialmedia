f503fbfcc54d6362a0d0673255ab515444897fd5
Kaustubh <kaustubhthakur76@gmail.com>
2025-02-06 00:14:44 +0530

    first commit

---
+++ backend/controllers/users.js
@@ -0,0 +1,9 @@
+const User = require('../models/User')
+const getUser = async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(201).json(user);
+    } catch (error) {
+        console.error(error);
+    }
+}
