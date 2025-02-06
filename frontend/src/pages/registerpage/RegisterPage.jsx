343542eda7e576d378f496ba65f6eeff336861cf
Kaustubh <kaustubhthakur76@gmail.com>
2025-02-06 23:42:44 +0530

    first commit

--- frontend/src/pages/registerpage/RegisterPage.jsx
+++ frontend/src/pages/registerpage/RegisterPage.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from 'react'
 import {Link} from "react-router-dom"
 const RegisterPage = () => {
-    const [username,setUsername] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const register = async(e)=>{
-        try {
-            
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    const [inputs, setInputs] = useState({
+		username: "",
+		email: "",
+		password: "",
+	});
+    const handleSignup = async () => {
+		try {
+			const res = await fetch("http://localhost:7000/auth/register", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(inputs),
+			});
+			const data = await res.json();
+
+			if (data.error) {
+				showToast("Error", data.error, "error");
+				return;
+			}
+
+			localStorage.setItem("users", JSON.stringify(data));
+			setUser(data);
+		} catch (error) {
+			showToast("Error", error, "error");
+		}
+	};
   return (
     <div>
         Register baby
